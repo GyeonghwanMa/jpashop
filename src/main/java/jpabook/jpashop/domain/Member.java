0f04c5ed27919516dbfaa0227e64e2a29198155e
Gyeonghwan <ghma1213@gmail.com>
2022-05-10 09:04:45 +0900

    다양한 연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -9,12 +9,13 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "MEMBER_ID")
     private Long id;
+
     private String name;
     private String city;
     private String street;
     private String zipcode;
 
-    @OneToMany(mappedBy = "MEMBER_ID")
+    @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
